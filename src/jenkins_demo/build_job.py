0c18e329a0b014955980a62d611265c138ef1044
ytq <ytq12875@163.com>
2019-12-31 08:50:35 +0800

    日常提交

--- src/jenkins_demo/build_job.py
+++ src/jenkins_demo/build_job.py
@@ -26,18 +26,19 @@ class BuildJob:
 
     def do_build(self):
         while True:
-
-            job_name = input("请输入你要构建的Job名：多个Job请以英文的逗号隔开。\n ")
-            joblist = job_name.split(",")
-            threads = []
-            nloops = range(len(joblist))
-            for i in nloops:  # 根据输入的box进行多线程的创建
-                t = threading.Thread(target=self.thread_build, args=(i,joblist[i]))
-                threads.append(t)
-            for i in nloops:  # start threads 此处并不会执行线程，而是将任务分发到每个线程，同步线程。等同步完成后再开始执行start方法
-                threads[i].start()
-            for i in nloops:  # jion()方法等待线程完成
-                threads[i].join()
+            print("*" * 100)
+            job_name = input("请输入你要构建的Job名：多个Job请以英文的逗号隔开。\n 单个任务构建可以采取模糊查询匹配，多个任务建议键入任务全名！\n")
+            if job_name:
+                joblist = job_name.split(",")
+                threads = []
+                nloops = range(len(joblist))
+                for i in nloops:  # 根据输入的box进行多线程的创建
+                    t = threading.Thread(target=self.thread_build, args=(i,joblist[i]))
+                    threads.append(t)
+                for i in nloops:  # start threads 此处并不会执行线程，而是将任务分发到每个线程，同步线程。等同步完成后再开始执行start方法
+                    threads[i].start()
+                for i in nloops:  # jion()方法等待线程完成
+                    threads[i].join()
             else:
                 log.warning("用户选择退出！")
                 break
