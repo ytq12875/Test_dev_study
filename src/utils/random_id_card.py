4305880947f57652d45a0309085a1cd7870851f5
ytq <ytq12875@163.com>
2020-08-09 17:04:29 +0800

    直接使用bash获取当前时间

--- src/utils/random_id_card.py
+++ src/utils/random_id_card.py
@@ -12,13 +12,14 @@ class RandomIdCard:
         cur_path = os.path.dirname(os.path.dirname(__file__))
         csv_path = cur_path + "/utils/area.csv"
         self.filename = csv_path
+
     # 功能：从csv文件中读取一个区域编码字典
     # 输入：文件名称
     def __areaCodeDict(self):
         dataDict = {}
         key = 0
         value = 1
-        dataLine = open(self.filename,encoding='GBK').read().splitlines()
+        dataLine = open(self.filename, encoding='GBK').read().splitlines()
         for line in dataLine:
             tmpLst = line.split(",")
             dataDict[tmpLst[key]] = tmpLst[value]
@@ -26,7 +27,7 @@ class RandomIdCard:
 
     # 功能：随机生产一个区域码
 
-    def __areaCode(self,Dict):
+    def __areaCode(self, Dict):
         codeList = []
         for key in Dict.keys():
             codeList.append(key)
@@ -50,13 +51,13 @@ class RandomIdCard:
     # 功能：随机生成3位序列号
 
     def __ordrNum(self):
-        odNum = random.randint(100, 999)  # 随机生成100到999之间的3为数据
+        odNum = random.randint(0, 999)  # 随机生成0到999之间的数据
         sex = odNum % 2
-        return (str(odNum), sex)
+        return (str(odNum).zfill(3), sex) #返回3位补零的序列号和性别
 
     # 功能：生成校验码
 
-    def __check(self,id_num):
+    def __check(self, id_num):
         count = 0
         weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]  # 权重项
         checkcode = {'0': '1', '1': '0', '2': 'X', '3': '9', '4': '8', '5': '7', '6': '6', '7': '5', '8': '4', '9': '3',
@@ -66,27 +67,26 @@ class RandomIdCard:
         return checkcode[str(count % 11)]  # 算出校验码
 
     def get_id_card(self):
-        areaCodeDt = self.__areaCodeDict() # 调用生成字典
+        areaCodeDt = self.__areaCodeDict()  # 调用生成字典
         areaCd = self.__areaCode(areaCodeDt)  # 生成区域码
         areaCdName = areaCodeDt[areaCd]  # 获取区域名称
         birthDy = self.__birthDay()  # 生成出生日期
         (ordNum, sex) = self.__ordrNum()  # 生成顺序号和性别
         checkcode = self.__check((areaCd + birthDy + ordNum))  # 生产校验码
         id_card = areaCd + birthDy + ordNum + checkcode  # 拼装身份证号
-        return id_card,areaCdName
+        return id_card, areaCdName
 
-    def check_is_id_card(self,id_num):
+    def check_is_id_card(self, id_num):
         id_str = str(id_num)
         last_code = id_str[-1]
         true_code = self.__check(id_str[:-1])
         areaCodeDt = self.__areaCodeDict()  # 调用生成字典
         if last_code != true_code:
-            return "您查询的号码 "+id_str+" 不符合身份证规则"
+            return "您查询的号码 " + id_str + " 不符合身份证规则"
         else:
             prov = areaCodeDt[id_str[:6]]
             birth = id_str[6:10] + "年" + id_str[10:12] + "月" + id_str[12:14] + "月"
-            return "您查询的号码 "+ id_str + " 符合身份证规则。此人来自于："+ prov + " 出生于："+ birth + " 。"
-
+            return "您查询的号码 " + id_str + " 符合身份证规则。此人来自于：" + prov + " 出生于：" + birth + " 。"
 
 
 if __name__ == '__main__':
